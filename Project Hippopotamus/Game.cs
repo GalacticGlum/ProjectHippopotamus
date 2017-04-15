b2c2999aac66fe52888702aa3c8a14729e5c4ec2
GalacticGlum <nerftactic09@gmail.com>
2017-04-15 17:29:01 -0400

    UI redo.

--- Project Hippopotamus/Game.cs
+++ Project Hippopotamus/Game.cs
@@ -9,19 +9,12 @@ namespace Hippopotamus
     public class Game : GameInstance
     {
         private float timer;
-        private Widget testWidget;
         private Texture2D blankTexture;
 
         public override void Initialize()
         {
             Context.IsMouseVisible = true;
             EntitySystemManager.Register<WorldSystem>();
-
-            Widget root = new Widget(new Vector2(Context.Width, Context.Height), Anchor.TopLeft);
-            testWidget = new Widget(new Vector2(500, 32), Anchor.Centre)
-            {
-                Parent = root
-            };
 
 
             blankTexture = new Texture2D(Context.GraphicsDevice, 1, 1);
@@ -37,17 +30,10 @@ namespace Hippopotamus
         public override void Update(GameLoopEventArgs args)
         {          
             timer += args.DeltaTime;
-            testWidget.Update();
-
             if (!(timer >= 1)) return;
 
             Context.Window.Title = $"FPS: {(int)Context.FramesPerSecond} | UPS: {(int)Context.UpdatesPerSeconds}";
             timer = 0;
-        }
-
-        public override void Draw(GameLoopEventArgs args)
-        {
-            testWidget.Draw(args.SpriteBatch, blankTexture);
         }
     }
 }

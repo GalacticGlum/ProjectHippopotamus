fe1efdf20e5bf224c9b2196d36d8b147a0857f50
GalacticGlum <nerftactic09@gmail.com>
2017-06-03 15:12:49 -0400

    Updated .gitignore and added .gitattributes

--- Assets/Game/Scripts/Bridge/LuaApiHandler.cs
+++ Assets/Game/Scripts/Bridge/LuaApiHandler.cs
@@ -2,30 +2,28 @@
 using System.Reflection;
 using MoonSharp.Interpreter;
 
-namespace Hippopotamus.Engine.Bridge
+
+internal static class LuaApiHandler
 {
-    internal static class LuaApiHandler
+    public static void Register()
     {
-        public static void Register()
+        // Expose all types decorated with the LuaExposeType attribute.
+        Type exposeAttributeType = typeof(LuaExposeType);
+        foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
         {
-            // Expose all types decorated with the LuaExposeType attribute.
-            Type exposeAttributeType = typeof(LuaExposeType);
-            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+            foreach (Type type in assembly.GetTypes())
             {
-                foreach (Type type in assembly.GetTypes())
+                if (Attribute.IsDefined(type, exposeAttributeType))
                 {
-                    if (Attribute.IsDefined(type, exposeAttributeType))
-                    {
-                        Lua.ExposeType(type);
-                    }
+                    Lua.ExposeType(type);
                 }
             }
-
-            // TODO: Move math to a Lua script. 
-            Lua.ExposeType(typeof(MathHelper));
-            Lua.ExposeType(typeof(Math));
-
-            UserData.RegisterAssembly();
         }
+
+        // TODO: Move math to a Lua script. 
+        Lua.ExposeType(typeof(MathHelper));
+        Lua.ExposeType(typeof(Math));
+
+        UserData.RegisterAssembly();
     }
 }
--- Assets/Game/Scripts/Bridge/LuaExposeType.cs
+++ Assets/Game/Scripts/Bridge/LuaExposeType.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 
-namespace Hippopotamus.Engine.Bridge
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum)]
+public class LuaExposeType : Attribute
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum)]
-    public class LuaExposeType : Attribute
-    {
-    }
 }

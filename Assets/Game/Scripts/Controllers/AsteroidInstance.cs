cc08c9e138bb1c43ca6fdb5c29ea117efc78c992
GalacticGlum <nerftactic09@gmail.com>
2017-07-01 17:51:21 -0400

    Fixed z-axis spawning on asteroids.

--- Assets/Game/Scripts/Controllers/AsteroidInstance.cs
+++ Assets/Game/Scripts/Controllers/AsteroidInstance.cs
@@ -13,7 +13,8 @@ public class AsteroidInstance : MonoBehaviour
 
     private void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        Vector2 movePosition = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        transform.position = new Vector3(movePosition.x, movePosition.y, -1);
         TerrainUtilities.GenerateCircle(1, World.Current.WorldData, WorldController.Instance.WorldCoordiantesToGridSpace(transform.position), TileType.Empty);
 
         if (transform.position != targetPosition) return;
@@ -25,7 +26,8 @@ public class AsteroidInstance : MonoBehaviour
         float size = impactRadius / 2f;
         Vector2i impactPosition = WorldController.Instance.WorldCoordiantesToGridSpace(transform.position + new Vector3(0, size));
         TerrainUtilities.GenerateFuzzyCircle(impactRadius, impactRadius + 2, World.Current.WorldData, impactPosition, TileType.Empty);
-        GameObject impactGameObject = Instantiate(impactPrefab, transform.position, Quaternion.identity);
+
+        GameObject impactGameObject = Instantiate(impactPrefab, transform.position + new Vector3(0, 0, -1), Quaternion.identity);
         impactGameObject.transform.localScale = new Vector3(size, size, size);
 
         Destroy(impactGameObject, impactGameObject.GetComponent<ParticleSystem>().main.duration);
@@ -37,6 +39,7 @@ public class AsteroidInstance : MonoBehaviour
         float angle = Random.Range(40, -40)  * Mathf.Deg2Rad;
         int distance = Mathf.FloorToInt(Camera.main.orthographicSize * 3);
 
+        targetPosition.z = -1;
         Vector3 spawnPosition = targetPosition + new Vector3(Mathf.Sin(angle) * distance, Mathf.Cos(angle) * distance, -1);
         GameObject instance = Instantiate(prefab, spawnPosition, Quaternion.identity);
         AsteroidInstance asteroidInstance = instance.AddComponent<AsteroidInstance>();
